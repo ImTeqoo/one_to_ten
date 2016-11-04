e49c6c613330d652ff0f44bc8d711956a32263bf
Talon Navarez <navareztalon@gmail.com>
2016-11-04 09:19:42 -1000

    Butterflies can dive

---
+++ apps.js
@@ -0,0 +1,59 @@
+// One_To_Ten
+var hero = 'Muffin Man';
+var gender = 'Male';
+var age = 32;
+var weapon = 'Exploding chocolate muffins with percing macadamia nuts';
+var pronoun; 
+var xp;
+// conditional to set pronoun
+if(gender === 'Male'){
+	pronoun = 'his'
+}else if(gender === 'female'){
+	pronoun = 'her'
+}else{
+	pronoun = 'their'
+}
+
+// conditional to set age 
+if(age <= 18){
+	xp = 'is just beginning';
+}
+
+if(age >= 21 && age <= 40){
+	xp = 'has seen a few battles'
+}
+
+if(age > 40){
+	xp = 'has lived through many epic battles'
+}
+
+// 1st statement
+console.log(hero + 'is our lovable hero, who' xp + 'with his trusty' weapon + '!!!')
+
+
+
+
+
+
+
+//1 Dish 2 Dish
+var count;
+var Food = 'spaghetti'
+
+function dishHouse (s,t){
+	count = s + t;
+	console.log(count)
+}
+dishHouse (7,2)
+
+//1 Dish 2 Dish (cont.)
+
+//Assemble the Avengers
+var hero = 'Hulk'
+
+function avengersAssemble (g){
+	if()
+}
+//Lights Out
+
+//Vroom Vroom
