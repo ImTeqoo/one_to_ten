5c3ceb5073f95ad3343daf3d4143692ac77bcd71
Talon Navarez <navareztalon@gmail.com>
2016-11-21 15:38:29 -1000

    fruit basket

--- apps.js
+++ apps.js
@@ -30,12 +30,6 @@ if(age > 40){
 // 1st statement
 console.log(hero + 'is our lovable hero, who' xp + 'with his trusty' weapon + '!!!')
 
-
-
-
-
-
-
 //1 Dish 2 Dish
 var count;
 var Food = 'spaghetti'
@@ -47,13 +41,37 @@ function dishHouse (s,t){
 dishHouse (7,2)
 
 //1 Dish 2 Dish (cont.)
-
+	if(count > 2){
+		console.log('we have to many' + food + 's.')
+	}else{
+		console.log('welcome to the club!!')
+	}
 //Assemble the Avengers
 var hero = 'Hulk'
 
 function avengersAssemble (g){
-	if()
-}
+	if(hero = Hulk){
+		console.log(Hulk + 'smash')
+	}else if(hero = black widow){
+		console.log(black widow + 'I cannot let you do that...')
+	}else if(hero = ironman){
+		console.log(ironman + 'So was I...')
+	}else if(hero = captain america){
+		console.log(captain america + 'He is my friend...')
+	}else{
+		console.log('HEY! YOU THERE! YAH! THERE! *whispers* youre not needed over there.')
+	}
+} 
+avengersAssemble (Hulk)
 //Lights Out
+var lights = true 
 
+function LightsOut (t){
+	if(lights = true){
+		console.log('And then there was light.')
+	}else{
+		console.log('Lights out.')
+	}
+}
 //Vroom Vroom
+var carType = Toyota 
